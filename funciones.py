03130ad31a52bbf39f7031be3b44c37f548e0eb3
Alberto Alonso Moreno <78491659+Tuch0@users.noreply.github.com>
2021-11-13 16:30:09 +0100

    Actualización de PassGenerator

--- funciones.py
+++ funciones.py
@@ -1,26 +1,58 @@
 #!bin/bash/python3
-
 from random import choice
+from sys import stdout
 
+# Colores:
+def red():
+    RED = "\033[1;31m"
+    stdout.write(RED)
+
+def green():
+    GREEN = "\033[0;32m"
+    stdout.write(GREEN)
+
+def blue():
+    BLUE = "\033[1;34m"
+    stdout.write(BLUE)
+
+def yellow():
+    YELLOW = "\033[1;33m"
+    stdout.write(YELLOW)
+
+def purple():
+    PURPLE = "\033[1;35m"
+    stdout.write(PURPLE)
+
+def white():
+    WHITE = "\033[1;37m"
+    stdout.write(WHITE)
+
+# Funciones
 def letras():
+    purple()
     longitud = int(input('Introduce la longitud de la contraseña: '))
     valores = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
     a = ""
     a = a.join([choice(valores) for i in range(longitud)])
+    white()
     print(a)
 
 
 def numeros():
+    purple()
     longitud = int(input('Introduce la longitud de la contraseña: '))
     valores = "0123456789"
     a = ""
     a = a.join([choice(valores) for i in range(longitud)])
+    white()
     print(a)
 
 
 def letras_numeros():
+    purple()
     longitud = int(input('Introduce la longitud de la contraseña: '))
     valores = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789"
     a = ""      
     a = a.join([choice(valores) for i in range(longitud)])
+    white()
     print(a)
---
+++ generador.py
@@ -0,0 +1,64 @@
+import time
+from sys import stdout
+import funciones as f
+
+def red():
+    RED = "\033[1;31m"
+    stdout.write(RED)
+
+def green():
+    GREEN = "\033[0;32m"
+    stdout.write(GREEN)
+
+def blue():
+    BLUE = "\033[1;34m"
+    stdout.write(BLUE)
+
+def yellow():
+    YELLOW = "\033[1;33m"
+    stdout.write(YELLOW)
+
+def purple():
+    PURPLE = "\033[1;35m"
+    stdout.write(PURPLE)
+
+def white():
+    WHITE = "\033[1;37m"
+    stdout.write(WHITE)
+
+banner = """
+______              _____                           _             
+| ___ \            |  __ \                         | |            
+| |_/ /_ _ ___ ___ | |  \/ ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
+|  __/ _` / __/ __|| | __ / _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
+| | | (_| \__ \__ \| |_\ \  __/ | | |  __/ | | (_| | || (_) | |    (By Tuch0)
+\_|  \__,_|___/___(_)____/\___|_| |_|\___|_|  \__,_|\__\___/|_|                                                            
+"""
+
+def menu():
+    red()
+    print(banner)
+    blue()
+    time.sleep(1)
+    print("1 -> Solo Letras")
+    time.sleep(0.5)
+    print("\n2 -> Solo Números")
+    time.sleep(0.5)
+    print("\n3 -> Letras y Números")
+    time.sleep(0.5)
+    print("\n4 -> Salir")
+    time.sleep(0.5)
+
+menu()
+option = int(input("\n-->> "))
+
+if option == 1:
+    f.letras()
+elif option == 2:
+    f.numeros()
+elif option == 3:
+    f.letras_numeros()
+elif option == 4:
+    exit
+else:
+    print('El dato introducido es inválido , prueba de nuevo solo introduciendo números enteros')
